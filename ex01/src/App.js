9a5ae921ec24c0cda60cfde8059d519e8aaae26b
eldanka <eldankahrovic2208@gmail.com>
2021-03-31 21:38:23 +0200

    Adding ex01

---
+++ ex01/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="App">
+      <button onClick={this.setMyStorage}>My Storage</button>
+    </div>
+    )
+  }
+
+  setMyStorage() {
+    document.cookie = "Year=2021";
+    localStorage.setItem('Paragon', 'yes, but Arena first');
+    sessionStorage.setItem('frontend', 'React');
+  }
+}
+
+export default App;
